f6e2a8dc4f3f4cb4d6172915df764b3481073089
zouhaier slimane <zouhaier.slimane@biat.com.tn>
2018-05-30 14:20:07 +0100

    router changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PostsListComponent } from './components/posts-list/posts-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+
+
+const my_routes = [
+  {path: 'list', component: PostsListComponent} ,
+  {path: 'welcome', component: WelcomeComponent} ,
+  {path: '', redirectTo: './welcome', pathMatch: 'full'} ,
+  {path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsListComponent
+    PostsListComponent,
+    NavbarComponent,
+    FooterComponent,
+    WelcomeComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule, // since angular 4.3+
+    RouterModule.forRoot(my_routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
